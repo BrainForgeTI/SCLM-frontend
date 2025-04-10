55735d548ea004c49fe6731b4d94fba6f70c5e4a
Renan <tononjob@gmail.com>
2025-04-10 20:31:38 -0300

    :sparkles: feat(adding style in component MissionCard)

--- src/routes/SecondaryMissions/components/MissionsCard.tsx
+++ src/routes/SecondaryMissions/components/MissionsCard.tsx
@@ -15,15 +15,15 @@ export const MissionCard = (props: Props)=>{
             setReveal1(!reveal1);
     }
     return (
-        <div className={`flex flex-col h-[350px] w-[350px] justify-center text-center py-[20px] px-[40px] justify-items-center items-center ${reveal1 ? `${props.textColor} `:"text-white" } bg-${props.bgColor}/[.40] rounded-[40px] gap-[10px]`}  onClick={switchEstilo1} >
+        <div className={`flex flex-col h-[350px] w-[350px] justify-center text-center py-[20px] px-[40px] justify-items-center items-center ${reveal1 ? `${props.textColor} rotate-y-[180deg]`:"text-white rotate-y-[-180deg]" } bg-${props.bgColor}/[.40] rounded-[40px] gap-[10px] transition-transform duration-1000 transform-style-preserve-3d `}  onClick={switchEstilo1} >
             <img className={`w-[200px] ${reveal1 ? "hidden":""}`} src={props.image} alt="" />
-            <img className={`flex w-[45px] text-center ${reveal1 ? "":"hidden"}`} src={props.imageIcon} alt="" />
-            <p className={reveal1 ? "":"hidden"}>{props.textMission}</p>
-            <div className="flex justify-center gap-[10px]">
+            <img className={`flex w-[45px] text-center ${reveal1 ? "rotate-y-[180deg]":"hidden"}`} src={props.imageIcon} alt="" />
+            <p className={reveal1 ? "rotate-y-[180deg]":"hidden"}>{props.textMission}</p>
+            <div className="flex justify-center gap-[10px] rotate-y-[180deg]    ">
                 <p className={`${reveal1 ? "":"hidden"} `}>{props.xpMission}</p>
                 <p className={`${reveal1 ? "":"hidden"} `}>{props.coinMission}</p>
             </div>
-            <button onClick={switchEstilo1} className="text-white">Clique para revelar</button>
+            <button onClick={switchEstilo1} className="text-white rotate-y-[180deg]">Clique para revelar</button>
         </div>
     )
 }
