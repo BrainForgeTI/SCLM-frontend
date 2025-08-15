f82ecc549c543e0e1bb0f65c03fd3790b0fb0028
kaikdorvalo <kaikdorvalo@gmail.com>
2025-08-15 17:35:00 -0300

    :recycle: refact(signin): refact signin components and colors

--- src/routes/SignIn/SignInPage.tsx
+++ src/routes/SignIn/SignInPage.tsx
@@ -7,80 +7,97 @@ import { Link } from "react-router";
 import { NavLink } from "react-router";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
 export const SignInPage = () => {
-    const [loginForm,setLoginForm] = useState({
-        email:"",
-        password:""
-    })
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: ""
+  })
 
-    function handleInputValue(event:ChangeEvent<HTMLInputElement>,fieldId:string){
+  function handleInputValue(event: ChangeEvent<HTMLInputElement>, fieldId: string) {
 
-        setLoginForm((prev)=>({
-            ...prev,
-            [fieldId]:event.target.value
-        }))
+    setLoginForm((prev) => ({
+      ...prev,
+      [fieldId]: event.target.value
+    }))
+  }
+  const [emailValue, setEmailValue] = useState<AuthStepFields>(
+    {
+      fieldId: "email",
+      fieldLabel: "Email",
+      fieldPlaceholder: "Seu Email",
+      fieldType: "email",
+      fieldValidator: () => { return true }
     }
-    const [emailValue,setEmailValue] = useState<AuthStepFields>(
-        {
-            fieldId:"email",
-            fieldLabel:"Email",
-            fieldPlaceholder:"Seu Email",
-            fieldType:"email",
-            fieldValidator:()=>{return true}
-        }
-    )
+  )
 
-    const [senhaValue,setSenhaValue] = useState<AuthStepFields>(
-        {
-            fieldId:"password",
-            fieldLabel:"Senha",
-            fieldPlaceholder:"Sua Senha",
-            fieldType:"password",
-            fieldValidator:()=>{return true}
-        }
-    )
-    useEffect(()=>{
-        console.log(loginForm)
-    },[loginForm])
-    return (<div>
-    
-            <PagelayoutAuth right={false} pageText={<p className="text-xl lg:w-[300px] xl:w-auto 2xl:w-auto text-end "> Aprenda, <span className="text-3xl 2xl:text-4xl text-primary font-bold">CONQUISTE,</span> evolua </p>} pageImage={Castle} imageAlt="Um castelo iluminado">
-                <div className="w-full flex justify-center lg:justify-end">
-                    <div className="w-[320px] lg:w-[384px] lg:block justify-center">
-                        <p className="flex justify-center w-full p-[20px] text-foreground text-[20px]">Entre no Scholarium</p>
-                        <AuthFormsBox>
-                            <div className="w-full flex flex-col gap-5">
-                                {/* <AuthFormsInput type={emailValue.fieldType} id={emailValue.fieldId} label={emailValue.fieldLabel} placeholder={emailValue.fieldPlaceholder} value={loginForm.email} handleInputValue={handleInputValue}></AuthFormsInput> */}
-                                <Input 
-                                    type={emailValue.fieldType}
-                                    id={emailValue.fieldId}
-                                    placeholder={emailValue.fieldPlaceholder}
-                                    // value={loginForm.email}
-                                    onChange={() => {}}
-                                />
-                                <Input 
-                                    id={senhaValue.fieldId}
-                                    type={senhaValue.fieldType}
-                                    placeholder={senhaValue.fieldPlaceholder}
-                                    // value={loginForm.password}
-                                />
-                                {/* <AuthFormsInput type={senhaValue.fieldType} id={senhaValue.fieldId} label={senhaValue.fieldLabel} placeholder={senhaValue.fieldPlaceholder} value={loginForm.password} handleInputValue={handleInputValue}></AuthFormsInput> */}
-                                {/* <button className="text-white w-full p-[10px] bg-primary rounded-lg cursor-pointer">Entrar</button> */}
-                                <Button className="p-5">Entrar</Button>
-                                <p className="flex justify-center w-full text-foreground text-base">Esqueceu a senha?<a className="flex ml-2 text-blue-400 underline" href="">clique aqui</a></p>
-                            </div>
-                            
-                        </AuthFormsBox>
-                        <div className="flex flex-col w-full mt-6 p-[15px] rounded-[20px] text-foreground justify-items-center text-center bg-neutral/5">
-                            <p>Ainda não tem uma conta?</p>
-                            <NavLink className="flex text-blue-400 underline text-center justify-center" to="/signup">Crie gratuitamente aqui</NavLink>
-                        </div>
-                    </div>
-                </div>
-            </PagelayoutAuth>
+  const [senhaValue, setSenhaValue] = useState<AuthStepFields>(
+    {
+      fieldId: "password",
+      fieldLabel: "Senha",
+      fieldPlaceholder: "Sua Senha",
+      fieldType: "password",
+      fieldValidator: () => { return true }
+    }
+  )
+  useEffect(() => {
+    console.log(loginForm)
+  }, [loginForm])
+  return (<div>
+
+    <PagelayoutAuth
+      right={false}
+      pageText={
+        <p className="text-xl lg:w-[300px] xl:w-auto 2xl:w-auto text-end text-foreground "> Aprenda,
+          <span className="text-3xl 2xl:text-4xl text-primary font-bold">CONQUISTE</span>, evolua
+        </p>
+      }
+      pageImage={Castle}
+      imageAlt="Um castelo iluminado"
+    >
+      <div className="w-full flex justify-center lg:justify-end">
+        <div className="w-[320px] lg:w-[384px] lg:block justify-center">
+          <p className="flex justify-center w-full p-[20px] text-foreground text-[20px]">Entre no Scholarium</p>
+          <Card>
+            <CardContent className="py-5 px-7 lg:px-10">
+              <div className="w-full flex flex-col gap-5">
+                <Input
+                  type={emailValue.fieldType}
+                  id={emailValue.fieldId}
+                  placeholder={emailValue.fieldPlaceholder}
+                  onChange={() => { }}
+                />
+                <Input
+                  id={senhaValue.fieldId}
+                  type={senhaValue.fieldType}
+                  placeholder={senhaValue.fieldPlaceholder}
+                />
+                <Button className="p-5">Entrar</Button>
+                <CardFooter className="flex justify-between flex-col lg:flex-row gap-2">
+                  <p className="text-foreground text-base">Esqueceu a senha?</p>
+                  <Link className="ml-2 text-blue-400 underline" to="">clique aqui</Link>
+                </CardFooter>
+              </div>
+            </CardContent>
+
+          </Card>
+          <Card className="p-5 flex justify-center mt-5">
+            <div>
+              <p className="text-center">Ainda não tem uma conta?</p>
+              <NavLink
+                className="flex text-blue-400 underline text-center justify-center"
+                to="/signup"
+              >
+                Crie gratuitamente aqui
+              </NavLink>
+            </div>
+          </Card>
         </div>
-        
-        );
+      </div>
+    </PagelayoutAuth>
+  </div>
+
+  );
 }
 
