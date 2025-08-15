8737f65755a130dd98031a22825610976f82df67
kaikdorvalo <kaikdorvalo@gmail.com>
2025-08-14 22:30:33 -0300

    :recycle: refact(signin): refact signin buttons

--- src/components/PagelayoutAuth/components/AuthFormsBox.tsx
+++ src/components/PagelayoutAuth/components/AuthFormsBox.tsx
@@ -8,7 +8,7 @@ const AuthFormsBox = (props: Props) => {
 
 
     return (
-        <form className="bg-neutral/5 rounded-[20px] p-8">
+        <form className="bg-card border border-ring/50 rounded-[20px] p-8">
             {props.children}
         </form>
     )
--- src/routes/SignIn/SignInPage.tsx
+++ src/routes/SignIn/SignInPage.tsx
@@ -5,6 +5,8 @@ import { AuthStepFields } from "../../types/auth_types/AuthStepField";
 import { ChangeEvent } from "react"
 import { Link } from "react-router";
 import { NavLink } from "react-router";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export const SignInPage = () => {
     const [loginForm,setLoginForm] = useState({
@@ -46,17 +48,31 @@ export const SignInPage = () => {
             <PagelayoutAuth right={false} pageText={<p className="text-xl lg:w-[300px] xl:w-auto 2xl:w-auto text-end "> Aprenda, <span className="text-3xl 2xl:text-4xl text-primary font-bold">CONQUISTE,</span> evolua </p>} pageImage={Castle} imageAlt="Um castelo iluminado">
                 <div className="w-full flex justify-center lg:justify-end">
                     <div className="w-[320px] lg:w-[384px] lg:block justify-center">
-                        <p className="flex justify-center w-full p-[20px] text-white text-[20px]">Entre no Scholarium</p>
+                        <p className="flex justify-center w-full p-[20px] text-foreground text-[20px]">Entre no Scholarium</p>
                         <AuthFormsBox>
                             <div className="w-full flex flex-col gap-5">
-                                <AuthFormsInput type={emailValue.fieldType} id={emailValue.fieldId} label={emailValue.fieldLabel} placeholder={emailValue.fieldPlaceholder} value={loginForm.email} handleInputValue={handleInputValue}></AuthFormsInput>
-                                <AuthFormsInput type={senhaValue.fieldType} id={senhaValue.fieldId} label={senhaValue.fieldLabel} placeholder={senhaValue.fieldPlaceholder} value={loginForm.password} handleInputValue={handleInputValue}></AuthFormsInput>
-                                <button className="text-white w-full p-[10px] bg-primary rounded-lg cursor-pointer">Entrar</button>
-                                <p className="flex justify-center w-full text-white text-[12px]">Esqueceu a senha?<a className="flex ml-2 text-blue-400 underline" href="">clique aqui</a></p>
+                                {/* <AuthFormsInput type={emailValue.fieldType} id={emailValue.fieldId} label={emailValue.fieldLabel} placeholder={emailValue.fieldPlaceholder} value={loginForm.email} handleInputValue={handleInputValue}></AuthFormsInput> */}
+                                <Input 
+                                    type={emailValue.fieldType}
+                                    id={emailValue.fieldId}
+                                    placeholder={emailValue.fieldPlaceholder}
+                                    // value={loginForm.email}
+                                    onChange={() => {}}
+                                />
+                                <Input 
+                                    id={senhaValue.fieldId}
+                                    type={senhaValue.fieldType}
+                                    placeholder={senhaValue.fieldPlaceholder}
+                                    // value={loginForm.password}
+                                />
+                                {/* <AuthFormsInput type={senhaValue.fieldType} id={senhaValue.fieldId} label={senhaValue.fieldLabel} placeholder={senhaValue.fieldPlaceholder} value={loginForm.password} handleInputValue={handleInputValue}></AuthFormsInput> */}
+                                {/* <button className="text-white w-full p-[10px] bg-primary rounded-lg cursor-pointer">Entrar</button> */}
+                                <Button className="p-5">Entrar</Button>
+                                <p className="flex justify-center w-full text-foreground text-base">Esqueceu a senha?<a className="flex ml-2 text-blue-400 underline" href="">clique aqui</a></p>
                             </div>
                             
                         </AuthFormsBox>
-                        <div className="flex flex-col w-full mt-6 p-[15px] rounded-[20px] text-white justify-items-center text-center bg-neutral/5">
+                        <div className="flex flex-col w-full mt-6 p-[15px] rounded-[20px] text-foreground justify-items-center text-center bg-neutral/5">
                             <p>Ainda não tem uma conta?</p>
                             <NavLink className="flex text-blue-400 underline text-center justify-center" to="/signup">Crie gratuitamente aqui</NavLink>
                         </div>
