8841ab68ef1cdc82b04361fc419a9554bfc28b78
Renan <tononjob@gmail.com>
2025-09-10 20:29:38 -0300

    feat(notebook): update component ReactMarkdown

--- src/routes/Notebook/NotebookPage.tsx
+++ src/routes/Notebook/NotebookPage.tsx
@@ -47,8 +47,8 @@ export const NotebookPage = () => {
                 components={{
                 h1: ({...props}) => <h1 className="text-[32px] font-bold mb-4 text" {...props} />,
                 h2: ({...props}) => <h2 className="text-[24px] font-semibold mt-6 mb-2" {...props} />,
-                h3: ({...props}) => <h2 className="text-[20px] font-semibold mt-6 mb-2" {...props} />,
-                p:  ({...props}) => <h2 className="text-[16px] mt-6 mb-2" {...props} />,
+                h3: ({...props}) => <h3 className="text-[20px] font-semibold mt-6 mb-2" {...props} />,
+                p:  ({...props}) => <p className="text-[16px] mt-6 mb-2" {...props} />,
                 ul: ({...props}) => <ul className="list-disc ml-6" {...props} />,
                 ol: ({...props}) => <ol className="list-decimal ml-6" {...props} />,
             }}
