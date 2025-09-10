7e9787e29d9f34cdba032e1c003294b3df86e3bf
Renan <tononjob@gmail.com>
2025-09-10 20:10:12 -0300

    feat(notebook): switch render in div to markdown

--- src/routes/Notebook/components/ModeloAnotacoes.tsx
+++ src/routes/Notebook/components/ModeloAnotacoes.tsx
@@ -8,12 +8,12 @@ interface Props {
 
 export const ModeloAnotacoes = (props:Props) => {
     return (
-    <div className="flex flex-col py-[10px]">
+    <div className="flex flex-col py-[10px] gap-[10px]">
         
         <div className="font-semibold text-[24px] pb-[5px] font">{props.subtitle}</div>
         <div className="text-[16px] text-white/80 pb-[5px]">{props.descrition}</div>
         <div>
-            <ul className="pb-[5px]">
+            <ul className="pb-[5px] gap-[5px]">
                 {props.topics?.map(item => (
                     <li className="list-disc italic text-[12px] text-white/80 mx-[20px]">{item}</li>
                 ))}
