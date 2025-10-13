ef1ad14106ebcfb48487c7578b39a92f7888afeb
Emerson Tanno <tannobrizotto@gmail.com>
2025-10-13 20:00:35 -0300

    :recycle: refactor(update-character): correcting update body

--- src/services/character/update-character.ts
+++ src/services/character/update-character.ts
@@ -1,14 +1,12 @@
 import { apiCharacter } from "@/lib/api-manager";
 import { CharacterInfoUptade } from "@/types/character/character-update";
 
-export async function updateCharacter (data: CharacterInfoUptade){
-
-    return (await apiCharacter.put(`characters/${data.id}`,{
-        characterName:data.characterName,
-        hairColor:data.hairColor,
-        hairIndex:data.hairIndex,
-        eyeIrisColor:data.hairIndex,
-        eyeIrisIndex:data.eyeIrisIndex,
-    })) 
-   
+export async function updateCharacter(data: CharacterInfoUptade) {
+  return await apiCharacter.put(`characters/${data.id}/appearance`, {
+    characterName: data.characterName,
+    hairColor: data.hairColor,
+    hairIndex: data.hairIndex,
+    eyeIrisColor: data.eyeIrisColor,
+    eyeIrisIndex: data.eyeIrisIndex,
+  });
 }
