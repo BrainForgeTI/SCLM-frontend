21f671750ebd1b71baa65c516f535525b807f334
Renan <tononjob@gmail.com>
2025-10-13 19:12:26 -0300

    refact(character): delete comments

--- src/services/character/delete-character.ts
+++ src/services/character/delete-character.ts
@@ -2,6 +2,6 @@ import { apiCharacter } from "@/lib/api-manager";
 import { CharacterDelete } from "@/types/character/character-delete";
 
 export async function deleteCharacter (character: CharacterDelete){
-    // return (await apiCharacter.delete(`characters/${character.id}`))
-    return {data: {data: {id:character.id}}}
+    return (await apiCharacter.delete(`characters/${character.id}`))
+   
 }
