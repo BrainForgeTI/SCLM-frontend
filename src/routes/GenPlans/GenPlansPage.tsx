25c0a44c28d77bb7bc8dd7871ff138a0d3eb2923
sirafonso <sirafonso005896@outlook.com>
2025-11-16 15:17:01 -0300

    :recycle: refactor: update plan names

--- src/routes/GenPlans/GenPlansPage.tsx
+++ src/routes/GenPlans/GenPlansPage.tsx
@@ -9,9 +9,9 @@ export const GenPlansPage = () => {
             <div className="flex flex-col gap-10">
                 <div className="text-[24px]">Planos</div>
                 <div className="grid grid-cols-1 md:grid-cols-2 justify-center gap-6">
-                    <PlanComponent nomePlan="Plano Explorador" description="Experimente o Atenium Gratuitamente e Descubra o Poder da IA na Educação!" estadoPlan={resultPlanBilling?.typePlan === TypePlans.FREE || resultPlanBilling?.typePlan === null} preco={"0"} keyPlan="price_1SMTZICgvpJPF4Xlfo6ViXGR"></PlanComponent>
-                    <PlanComponent nomePlan="Plano Estudioso" description="Aprofunde sua jornada com IA na educação!" estadoPlan={resultPlanBilling?.typePlan === TypePlans.ESTUDIOSO} preco={"29,00"} keyPlan="price_1SMRuDCgvpJPF4XlNBUGqB8V"></PlanComponent>
-                    <PlanComponent nomePlan="Plano Sábio" description="Domine o Atenium com IA avançada." estadoPlan={resultPlanBilling?.typePlan === TypePlans.PRO} preco={"69,90"} keyPlan="price_1SMSNaCgvpJPF4XlwZ4RYunT"></PlanComponent>
+                    <PlanComponent nomePlan="Plano Explorador" description="Experimente o Atenium Gratuitamente e Descubra o Poder da IA na Educação!" estadoPlan={resultPlanBilling?.typePlan === TypePlans.EXPLORER || resultPlanBilling?.typePlan === null} preco={"0"} keyPlan="price_1SMTZICgvpJPF4Xlfo6ViXGR"></PlanComponent>
+                    <PlanComponent nomePlan="Plano Estudioso" description="Aprofunde sua jornada com IA na educação!" estadoPlan={resultPlanBilling?.typePlan === TypePlans.SCHOLAR} preco={"29,00"} keyPlan="price_1SMRuDCgvpJPF4XlNBUGqB8V"></PlanComponent>
+                    <PlanComponent nomePlan="Plano Sábio" description="Domine o Atenium com IA avançada." estadoPlan={resultPlanBilling?.typePlan === TypePlans.WISE} preco={"69,90"} keyPlan="price_1SMSNaCgvpJPF4XlwZ4RYunT"></PlanComponent>
                     <PlanComponent nomePlan="Plano Luminar" description="Evolua com IA de ponta." estadoPlan={resultPlanBilling?.typePlan === TypePlans.LUMINAR} preco={"259,60"} keyPlan="price_1SMSOSCgvpJPF4XlDl4BO9D2"></PlanComponent>
                 </div>
             </div>
